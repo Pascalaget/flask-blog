074f13444a7fc608c7f62cc03d26cfa68e5b9de0
pascalaget <pascal.agethen@gmail.com>
2019-01-17 13:15:36 +0100

    Appengine version

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Filme.db'
+
+CLOUDSQL_CONNECTION = os.environ.get('CLOUDSQL_CONNECTION')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = CLOUDSQL_CONNECTION
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
--- run.py
+++
@@ -1,9 +0,0 @@
-from blog import app
-import blog.movieserver as movieserver
-from multiprocessing import Process
-
-if __name__ == '__main__':
-    p0 = Process(target = movieserver.M)
-    p0.start()
-    p1 = Process(app.run(debug=True))
-    p1.start()
