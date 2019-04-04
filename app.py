887dcf41325711b29a38a9a3dc96e4f9d27d4215
pascalaget <pascal.agethen@gmail.com>
2019-04-04 21:08:22 +0200

    updated

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from blog import app
-import blog.movieserver as movieserver
-from multiprocessing import Process
 
 if __name__ == '__main__':
-    p0 = Process(target = movieserver.M)
-    p0.start()
-    p1 = Process(app.run(debug=True))
-    p1.start()
+    app.run(debug=True).start()
